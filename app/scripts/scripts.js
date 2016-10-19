a1a8f810f65c5b613f567a6873a076d5f2278b6c
Steven Nagie <sanagie@yahoo.com>
2016-10-19 17:06:11 -0600

    Some stuff

---
+++ app/scripts/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+$('#newTaskForm').hide();
+
+  var listo = [];
+
+  function Task(task) {
+    this.task = task;
+    this.id = 'new';
+  }
+
+  var addTask = function(task) {
+    if(task) {
+      task = new Task(task);
+      listo.push(task);
+
+      $('#newItemInput').val('');
+      $('#newList').append(
+        '<a href="#finish" class="" id="item">' +
+        '<li class="list-group-item">' +
+        '<h3>' + task.task + '</h3>' +
+        '<span class="arrow pull-right">' +
+        '<i class="glyphicon glyphicon-arrow-right">' +
+        '</span>' +
+        '</li>' +
+        '</a>'
+      );
+    }
+  };
+
+
+
+
+});
